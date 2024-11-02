22d4bf757efe764540920afd6cfe9fbe88bb9de6
ihor_hozhenko <lugtrauma@gmail.com>
2024-11-02 21:02:46 +0000

    Soltion task-2

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,13 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = images
+  .map(({ url, alt }) => `<li><img src="${url}" alt="${alt}" width="320"></li>`)
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
