6cf957d5e34b6727211b2ef7458110837e056ad9
ihor_hozhenko <lugtrauma@gmail.com>
2024-11-03 20:06:07 +0000

    Solution task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,21 @@
+// Функція для генерації випадкового кольору в hex-форматі
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Знаходимо елементи в DOM
+const bodyEl = document.body;
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+// Додаємо обробник події на кнопку
+changeColorButton.addEventListener("click", () => {
+  // Генеруємо новий випадковий колір
+  const randomColor = getRandomHexColor();
+
+  // Змінюємо колір фону <body> та оновлюємо текст у <span> з класом "color"
+  bodyEl.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
